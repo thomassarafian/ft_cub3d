1346e1807020508c93c2bf03a8fc27499830c659
ThomasSarafian <thomassarafian@gmail.com>
2020-06-03 21:19:28 +0200

    oui

--- cub3d.c
+++ cub3d.c
@@ -21,6 +21,32 @@ void	free_stuff(t_cub *cub)
 	free(cub->parse.sprite);
 }
 
+int	expose_hook(void *params)
+{
+		// void	*params[3];
+	t_mlx	mlx;
+	t_info	infos;
+	t_cub	cub;
+
+	// void		**recup;
+	// t_info		*infos;
+	// t_mlx		*mlx;
+	// t_cub		*cub;
+
+	// infos = recup[0];
+	// mlx = recup[1];
+	// cub = recup[2];
+	printf("NOOON\n");
+	if ("")
+		raycasting(&mlx, &infos, &cub);
+	
+	// all(((t_info *)recup[0]), ((t_mlx *)recup[1]), ((t_cub *)recup[2]));
+	
+	printf("coucouc\n");
+	// exit(0);
+	return 0;
+}
+
 int		main(int ac, char **av)
 {
 	void	*params[3];
@@ -32,7 +58,6 @@ int		main(int ac, char **av)
 		check_ext(av[1], ".cub");
 	else
 		ft_error("No map");
-	// form_parse(av[1]);
 	ft_start(&cub);
 	get_lines(&cub, &infos, av[1]);
 	fill_sp(&cub);
@@ -44,9 +69,13 @@ int		main(int ac, char **av)
 	params[2] = (void *)&cub;
 	raycasting(&mlx, &infos, &cub);
 	free_stuff(&cub);
-	// form_parse2(&cub, av[1]);
+	
+	
 	mlx_hook(mlx.win, 2, (1L << 0), keypressed, (void *)params);
 	mlx_hook(mlx.win, 17, (1L << 17), cross_destroy, (void *)(&params));
+	
+	mlx_expose_hook(mlx.win, expose_hook, (void *)params);
 	mlx_loop(mlx.mlx_ptr);
+	printf("WTF!!!\n");
 	return (0);
 }
